b1a3a91fb0a024d9253b780bb8d79de488905533
Mustafa Mohamed <rexosaur9@gmail.com>
2025-08-24 18:06:43 +0100

    This is my javascript course web development

---
+++ ch2_variables_datatypes/script.js
@@ -0,0 +1,12 @@
+var firstName = "Mustafe"
+var lastName = "Mohamed"
+ var age = 16
+var isStudent = true
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isStudent);
